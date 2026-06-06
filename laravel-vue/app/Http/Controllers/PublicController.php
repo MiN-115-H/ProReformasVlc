124ab0814e37774689f7ce6c9f4b2fc7d372c7e4
Minda <minhuamg@gmail.com>
2026-06-06 17:40:21 +0200

    Guard albums endpoint against invalid UTF-8

--- laravel-vue/app/Http/Controllers/PublicController.php
+++ laravel-vue/app/Http/Controllers/PublicController.php
@@ -5,34 +5,48 @@ namespace App\Http\Controllers;
 use App\Models\Album;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 class PublicController extends Controller
 {
     public function albums(): JsonResponse
     {
-        $albumes = Album::with(['fotos' => function ($query) {
-            $query->orderBy('orden');
-        }])->orderBy('nombre')->get()->map(function($album) {
-            return [
-                'id' => $album->id,
-                'nombre' => $album->nombre,
-                'descripcion' => $album->descripcion,
-                'categoria' => $album->categoria,
-                'fotos' => $album->fotos
-                    ->filter(fn ($foto) => filled($foto->url))
-                    ->map(function ($foto) {
-                        return [
-                            'id' => $foto->id,
-                            'url' => str_starts_with((string) $foto->url, 'http')
-                                ? $foto->url
-                                : url('/storage/' . ltrim((string) $foto->url, '/')),
-                            'descripcion' => $foto->descripcion,
-                        ];
-                    })
-                    ->values(),
-            ];
-        })->values();
+        try {
+            $albumes = Album::with(['fotos' => function ($query) {
+                $query->orderBy('orden');
+            }])->orderBy('nombre')->get()->map(function ($album) {
+                return [
+                    'id' => $album->id,
+                    'nombre' => $album->nombre,
+                    'descripcion' => $album->descripcion,
+                    'categoria' => $album->categoria,
+                    'fotos' => $album->fotos
+                        ->filter(fn ($foto) => filled($foto->url))
+                        ->map(function ($foto) {
+                            return [
+                                'id' => $foto->id,
+                                'url' => str_starts_with((string) $foto->url, 'http')
+                                    ? $foto->url
+                                    : url('/storage/' . ltrim((string) $foto->url, '/')),
+                                'descripcion' => $foto->descripcion,
+                            ];
+                        })
+                        ->values(),
+                ];
+            })->values();
 
-        return response()->json($albumes);
+            return response()->json(
+                $albumes,
+                200,
+                [],
+                JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE
+            );
+        } catch (Throwable $e) {
+            report($e);
+
+            return response()->json([
+                'message' => 'No se pudieron cargar los albums.',
+            ], 500, [], JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE);
+        }
     }
 }
